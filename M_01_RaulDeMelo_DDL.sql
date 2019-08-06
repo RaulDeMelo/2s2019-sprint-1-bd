26bb5ca5c5fe61d98adb54cf61114fb4467d786b
RaulDeMelo <48664318+RaulDeMelo@users.noreply.github.com>
2019-08-06 10:33:48 -0300

    Update M_01_RaulDeMelo_DDL.sql

--- M_01_RaulDeMelo_DDL.sql
+++ M_01_RaulDeMelo_DDL.sql
@@ -1,1 +1,20 @@
-a
+create database M_Gufos
+use M_Gufos
+create table Usuario(
+		IdUsuario int primary key identity not null
+		,Nome varchar (255) not null
+		,Email varchar (255) not null unique
+		,Senha varchar (255) not null
+		,Permissao varchar (255) not null
+);
+create table Categoria(
+		IdCategoria int primary key identity not null
+		,Nome varchar (255) not null unique
+);
+create table Evento(
+		IdEvento int primary key identity not null
+		,Titulo varchar (255) not null
+		,Descricao text
+		,DataEvento datetime not null
+		,
+)
