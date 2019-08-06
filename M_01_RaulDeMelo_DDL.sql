6984c180853e13f082623fb6117365fe5064bbb3
RaulDeMelo <48664318+RaulDeMelo@users.noreply.github.com>
2019-08-06 11:20:17 -0300

    Update M_01_RaulDeMelo_DDL.sql

--- M_01_RaulDeMelo_DDL.sql
+++ M_01_RaulDeMelo_DDL.sql
@@ -16,5 +16,11 @@ create table Evento(
 		,Titulo varchar (255) not null
 		,Descricao text
 		,DataEvento datetime not null
-		,
-)
+		,Localizacao varchar (255) null
+		,Ativo bit not null default(1)
+		,IdCategoria int foreign key references Categoria (IdCategoria)
+);
+create table Presencas(
+		IdUsuario int foreign key references Usuario (IdUsuario)
+		,IdEvento int foreign key references Evento (IdEvento)
+);
