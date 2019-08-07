312c1feaceb300db190cfa18d23c33081cb9c36c
RaulDeMelo <48664318+RaulDeMelo@users.noreply.github.com>
2019-08-07 08:05:58 -0300

    Update M_03_RaulDeMelo_DQL.sql

--- M_03_RaulDeMelo_DQL.sql
+++ M_03_RaulDeMelo_DQL.sql
@@ -1,1 +1,28 @@
-a
+use M_Gufos
+select * from Categoria order by IdCategoria asc;
+select * from Evento order by DataEvento desc;
+select * from Usuario;
+select * from Presencas;
+
+-- INSERÇÃO DE CATEGORIA-NOME (RELAT-EVENTO-CATEGORIA)
+select E.*, C.*
+from Evento E
+inner join Categoria as C
+on E.IdCategoria = C.IdCategoria;
+
+-- INSERÇÃO DE USUARIO-EVENTO (RELAT-PRESENÇAS-USUÁRIO)
+select P.*, U.*, E.*
+from Presencas as P
+inner join Usuario as U
+on P.IdUsuario = U.IdUsuario
+join Evento as E
+on P.IdEvento = E.IdEvento;
+
+select P.*, U.*, E.*, C.*
+from Presencas as P
+join Usuario as U
+on P.IdUsuario = U.IdUsuario
+join Evento as E
+on P.IdEvento = E.IdEvento
+join Categoria as C
+on E.IdCategoria = C.IdCategoria;
