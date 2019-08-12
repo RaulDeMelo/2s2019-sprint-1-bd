a37b50800dea9137eb35dc6f61919d3eb9c68e07
RaulDeMelo <48664318+RaulDeMelo@users.noreply.github.com>
2019-08-12 11:28:45 -0300

    Rename M_02_RaulDeMelo_DQL.sql to M_03_RaulDeMelo_DQL.sql

--- ExOpflix/M_03_RaulDeMelo_DQL.sql
+++ ExOpflix/M_03_RaulDeMelo_DQL.sql
@@ -2,13 +2,13 @@ use M_Opflix
 
 -- VISUALIZAR TODO TIPO DE USUARIO
 select * from TipoUsuario
--- VISUALIZAR TODOS OS USU¡RIOS
+-- VISUALIZAR TODOS OS USU√ÅRIOS
 select * from Usuario
 -- VISUALIZAR TODO TIPO DE METRAGEM
 select * from TipoMetragem
 -- VISUALIZAR TODAS AS CATEGORIAS
 select * from Categoria
--- VISUALIZAR TODOS OS LAN«AMENTOS
+-- VISUALIZAR TODOS OS LAN√áAMENTOS
 select * from Lancamento
 -- VISUALIZAR TODOS OS FAVORITADOS
 select * from LancamentoFavoritado 
